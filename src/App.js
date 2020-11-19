0572ba16bfb31cc8298558e988f354b7b7fe7ed7
Jean Louis <jeanlouis.dossantos84@gmail.com>
2020-11-19 17:00:12 +0100

    second commit, modif de App pour passage en classe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Welcome from './Components/Welcome'
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return   <Welcome user='you'/>
-}
+class App extends React.Component {
+
+  render(){
+  return   (<div>
+  <Welcome user='you'/>
+  <Welcome user='toi'/>
+  <Welcome user='nous'/>
+  </div>
+  ) }
+  
+}; 
 
 export default App;
